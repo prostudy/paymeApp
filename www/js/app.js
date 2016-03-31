f611a553f1188b045c8028d245cfab1de6e3d2aa
prostudy <pmstudykit@gmail.com>
2016-03-31 17:09:14 -0600

    Se agregaron funciones de navigator.globalization para hacer pruebas

--- www/js/app.js
+++ www/js/app.js
@@ -28,7 +28,21 @@ StarterModule.run(function($ionicPlatform,$translate) {
                 console.log("ERROR -> " + error);
             });
         }, null);
+        
+        navigator.globalization.getLocaleName(
+        	    function (locale) {alert('locale: ' + locale.value + '\n');},
+        	    function () {alert('Error getting locale\n');}
+        	);
+        
+        navigator.globalization.dateToString(
+        	    new Date(),
+        	    function (date) { alert('date: ' + date.value + '\n'); },
+        	    function () { alert('Error getting dateString\n'); },
+        	    { formatLength: 'short', selector: 'date and time' }
+        	);
     }
+    
+    
   });
 })
 
